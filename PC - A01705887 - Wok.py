007f08ab3094db6d2bb0c5269a186a12c014a3e4
A01705887 <111073706+alexysreyna@users.noreply.github.com>
2022-08-25 21:24:08 -0500

    Archivo .py
    Le faltan muchos detalles por agregar. Yo aprendí python hace tiempo y recuerdo que por medio de tkinter se le puede dar desde color al fondo y a los textos, como también agregar imágenes y seleccionar múltiples datos por medio de herramientas; esto será muy útil para crear la interfaz de usuario de la aplicación que busco hacer.

---
+++ PC - A01705887 - Wok.py
@@ -0,0 +1,28 @@
+"""
+Este es un archivo python para el proyecto de pensamiento computacional
+A01705887
+Se pretende seguir el pseudocódigo una vez aprendidos los fundamentos
+"""
+
+user= input("Usuario: ")
+password= input("Contraseña: ")
+
+print("Bienvenido a la aplicación de Wok")
+print(user)
+
+print("¿Usted desea ordenar un Wok?")
+
+"""
+Con la debida opción se le propondrá al usuario poder seleccionar "sí" o "no"
+Si se selecciona "Sí" entonces se le desplegará el menú
+"""
+
+sencillo= 85
+preparado= 94
+fuze_tea= 20
+agua= 15
+agua_chica= 15
+
+"""
+Dependiendo de la selección del usuario se sumará el precio total
+"""
